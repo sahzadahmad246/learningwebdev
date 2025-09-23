dc999b37ec704fc06d66177a5e4cb41f1a96e758
Shahzad Ahmad <sahzadahmad0001@gmail.com>
2025-09-23 18:23:02 +0530

    lecture 40 done

---
+++ lecture40/index.js
@@ -0,0 +1,28 @@
+// var age = 25;
+
+// if(true){
+//     console.log(age)
+// }
+
+// let name = "shahzad";
+// console.log(name);
+
+// let a = "shahzad"
+// a = 23
+// a = true
+// a=null
+// console.log(a)
+
+// const 32a343434fsddf = 29;
+// // a=20
+// // const a = 20
+// console.log(a)
+
+// console.log(a)
+// if(true){
+
+// const a = 12
+// const a =3
+// }
+let name = null;
+console.log(typeof null)
