68cddb437e571a944cdd4ab70184acd86897cb48
Shahzad Ahmad <sahzadahmad0001@gmail.com>
2025-09-26 01:11:03 +0530

    lecture 43 done

---
+++ lecture43/script.js
@@ -0,0 +1,104 @@
+// function sayMyName() {
+//   console.log("hi");
+// }
+// sayMyName();
+
+// function countToTen() {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+// }
+
+// countToTen();
+// let intro = "im shahzad ahmad";
+// function takingPara(num) {
+//   console.log("printing parameter:", num);
+// }
+
+// takingPara(intro);
+
+// function getAverage(para1, para2) {
+//   const average = (para1 + para2) / 2;
+//   console.log("Average is:", average);
+// }
+// getAverage(4, 76);
+// function getSum(a, b, c) {
+//   let sum = a + b + c;
+//   return sum;
+// }
+
+// let answer = getSum(3, 2, 5);
+// console.log("answer is:", answer);
+
+// function myName(firstName, lastName) {
+//   let fullName = firstName + " " + lastName;
+//   return fullName;
+// }
+
+// console.log("My Name is:", myName("Shahzad", "Ahmad"));
+
+// let multiplySir = function (val, val2) {
+//   return val * val2;
+// };
+
+// console.log("answer is:", multiplySir(4, 10));
+
+// let getSquare = function (val1) {
+//   return val1 ** 2;
+// };
+// console.log("answer is:", getSquare(10));
+
+// let getPower = (val1, val2) => {
+//   return val1 ** val2;
+// };
+// console.log(getPower(5, 2));
+
+// function greet(name) {
+//   console.log("Salam", name);
+// }
+// greet("Shahzad Ahmad");
+
+// function getSquare(num) {
+//   return num ** 2;
+// }
+// console.log(`squre is ${getSquare(5)}`);
+
+// let getSum = function (num1, num2) {
+//   return num1 + num2;
+// };
+// let sum = getSum(100, 200);
+// console.log(sum);
+
+// let multiplySir = (para1, para2 = 2) => {
+//   return para1 * para2;
+// };
+// let multiplySir = (para1, para2 = 4) => para1 * para2;
+// console.log(multiplySir(50));
+// console.log(multiplySir(4, 5));
+
+// const capitalizeName = (name) => {
+//   return name.charAt(0).toUpperCase() + name.slice(1);
+// };
+// console.log(capitalizeName("shahzad"));
+
+// const reverseString = (str) => {
+//   return str.split("").reverse().join("");
+// };
+// console.log(reverseString("shahzad"));
+
+// let isPalindrome = (word) => {
+//   let normalWord = word.split("");
+//   let isPalindromeWord = normalWord.reverse().join("");
+//   return word === isPalindromeWord;
+// };
+// console.log(isPalindrome("hello"));
+// console.log(isPalindrome("madam"));
+
+// let getfactorialNum = (num) => {
+//   let result = 1;
+//   for (let i = num; i > 0; i--) {
+//     result *= i;
+//   }
+//   return result;
+// };
+// console.log(getfactorialNum(5));
