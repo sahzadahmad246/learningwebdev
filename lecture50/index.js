72cc59d38d33cb17b7a14bf1fd7ddb719830a588
Shahzad Ahmad <sahzadahmad0001@gmail.com>
2025-10-04 01:06:35 +0530

    lecture50 done

---
+++ lecture50/index.js
@@ -0,0 +1,21 @@
+// errros
+
+// complie time error
+
+// SyntaxError
+// console.log(x
+
+// runtime error
+
+// console.log(x)
+
+try {
+  console.log("try block");
+  console.log(x);
+} catch (err) {
+  console.log(" error block");
+  console.log("erro is",err)
+//   throw new Error("define x pls");
+} finally {
+  console.log("ill run every time");
+}
---
+++ lecture51/index.js
@@ -0,0 +1,17 @@
+// let heading = document.getElementById("heading");
+// heading.innerText = "this is a heading";
+// console.log(heading);
+
+// let para = document.getElementsByClassName("paragraph");
+//  let para = document.getElementsByTagName("p");
+// let para = document.querySelector(".paragraph");
+// let para = document.querySelectorAll(".paragraph");
+// console.log(para);
+
+// let para = document.querySelector("#para-box");
+// para.innerHTML = '<h1 class="para">now heading.</h1>'
+// console.log(para.innerHTML);
+
+let para = document.querySelector(".para");
+// para.outerHTML = '<h1 class="para">now heading.</h1>'
+console.log(para.outerHTML);
