bb588dfe49338d27cb09be4004dcddcba9a6ed60
Shahzad Ahmad <sahzadahmad0001@gmail.com>
2025-10-23 01:57:33 +0530

    learned about useRef

--- REACT/react-mid/src/App.jsx
+++ REACT/react-mid/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Cart from "./components/Cart/Cart";
 import Counter from "./components/Counter";
+import ClickGame from "./components/useRef/ClickGame";
+import { Focus } from "./components/useRef/Focus";
+import Stopwatch from "./components/useRef/Stopwatch";
 // import TodoApp from "./components/TodoApp";
 
 function App() {
-  return <Cart />;
+  return <ClickGame />;
 }
 
 export default App;
